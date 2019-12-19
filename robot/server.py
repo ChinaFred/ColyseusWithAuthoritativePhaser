39aa57f5b5d2200d6357e60cb363fafbcf688f3d
chinafred <Juil0716>
2019-12-19 15:44:30 +0100

    Debugging path to images and scripts

--- robot/server.py
+++ robot/server.py
@@ -6,6 +6,8 @@ def start():
     config.display()
     config.current.app.run(debug=True, port=80, host='0.0.0.0')
     print("---------------------------------server started--------------------------------------")
+    print("app.root_path: " + config.current.app.root_path)
+    print("app.instance_path: "+config.current.app.instance_path)
 
 
 if __name__ == "__main__":
