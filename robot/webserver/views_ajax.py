3d6e83ced68bc3508325304de6d3bdf35b692de5
chinafred <Juil0716>
2019-12-19 16:30:59 +0100

    fixing directories name (again)

--- robot/webserver/views_ajax.py
+++ robot/webserver/views_ajax.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from robot import config
-from sensors.camera import test_action
+from sensors.camera import picam
 
 app = config.current.app
 
@@ -9,7 +9,7 @@ app = config.current.app
 def ajaxExample():
     print("ajax call success")
     print("Taking picture")
-    test_action.test_draw_image("monImage.png")
+    picam.shoot_photo("monImage.png")
     template = render_template("common/templates/cards/camera_card.html", config=config.current)
     print(template)
     return template
--- sensors/camera/picam.py
+++ sensors/camera/picam.py
@@ -1,10 +1,14 @@
 import time
 import picamera
+from robot import config
 
-with picamera.PiCamera() as camera:
-    print("take a picture")
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    # Camera warm-up time
-    time.sleep(2)
-    camera.capture('foo.jpg')
+
+def shoot_photo(filename):
+    with picamera.PiCamera() as camera:
+        print("take a picture")
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture(config.current.writePicturePath + filename)
+        config.current.set_lastPicture(filename)
