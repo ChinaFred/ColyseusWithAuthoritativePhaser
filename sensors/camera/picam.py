a5bc5f9d425f1348f81e93ce104714da440a2eee
chinafred <Juil0716>
2019-12-19 16:44:39 +0100

    Decrease camera resolution

--- sensors/camera/picam.py
+++ sensors/camera/picam.py
@@ -6,7 +6,7 @@ from robot import config
 def shoot_photo(filename):
     with picamera.PiCamera() as camera:
         print("take a picture")
-        camera.resolution = (1024, 768)
+        camera.resolution = (640, 480)
         camera.start_preview()
         # Camera warm-up time
         time.sleep(2)
