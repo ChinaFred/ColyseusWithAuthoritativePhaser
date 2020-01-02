e7bbe519ab138a1374c123647f858a9976df62ec
chinafred <Juil0716>
2020-01-02 15:11:55 +0100

    including servomotor.py

--- body/servos/servomotor.py
+++ body/servos/servomotor.py
@@ -1,5 +1,16 @@
 # coding: utf-8
 import RPi.GPIO as GPIO
+import time
+
+
+# vertical positions
+V_POS_CENTER = 6
+V_POS_TOP = 1
+V_POS_BOTTOM = 10
+# Horizontal positions
+H_POS_MIN = 2
+H_POS_MAX = 6
+H_POS_CENTER = 6
 
 
 class ServoMotor:
@@ -16,9 +27,23 @@ class ServoMotor:
 
     def go_to_position(self, pos):
         self.servo.ChangeDutyCycle(pos)
+        time.sleep(1)
 
 
+print("s")
 s = ServoMotor(22)
-s.go_to_position(8)
+s.go_to_position(V_POS_CENTER)
+s.go_to_position(V_POS_TOP)
+s.go_to_position(V_POS_BOTTOM)
+
+print("h")
+h = ServoMotor(27)
+h.go_to_position(H_POS_CENTER)
+h.go_to_position(H_POS_MIN)
+h.go_to_position(H_POS_MAX)
+
+s.go_to_position(V_POS_CENTER)
+h.go_to_position(H_POS_CENTER)
+GPIO.cleanup()
 
 
