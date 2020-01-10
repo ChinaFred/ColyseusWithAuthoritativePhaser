89e7f7e46e2b4674b7dcbbba3fb36653228e9c2c
chinafred <Juil0716>
2020-01-10 14:17:59 +0100

    Fix git conflit in getAlive

--- getAlive.py
+++ getAlive.py
@@ -1,10 +1,7 @@
-print("-------------------------------------------------------------------------------------")
-print("-------------------------starting application server---------------------------------")
-import brain.config as config
+# coding: utf-8
 import face.server as server
 
 
 if __name__ == "__main__":
-    server.start()
-    config.display()
-
+    s = server.Server()
+    s.start()
