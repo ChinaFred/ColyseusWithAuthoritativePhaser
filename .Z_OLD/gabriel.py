54e8e1ebf85c5793aa0054779ddd033cb21866aa
gabriel0430 <china.gabriel.04@gmail.com>
2020-01-12 21:33:23 +0100

    gabriel.py

--- .Z_OLD/gabriel.py
+++ .Z_OLD/gabriel.py
@@ -5,8 +5,16 @@ import RPi.GPIO as GPIO
 import time
 from AlphaBot import AlphaBot
 
+DR = 16
+DL = 19
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
+GPIO.setup(DR, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(DL, GPIO.IN, GPIO.PUD_UP)
 
 Ab = AlphaBot()
+
 
 
 for i in range(1, 4):
@@ -14,6 +22,10 @@ for i in range(1, 4):
     print("shsh")
 print("fdkjdfslkjsdklfjdslfkjdsfl")
 
-while True:
+DL_status = 1
+while DL_status:
+    DL_status = GPIO.input(DL)
+    time.sleep(1)
     Ab.forward()
+    print("av&nce")
 print("quoi? ")
