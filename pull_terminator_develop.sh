fc548bfa59d6569cd20c9a493a8c8cc8910d0385
chinafred <Juil0716>
2020-01-14 14:41:37 +0100

    New version of the IR REMOTE

--- pull_terminator_develop.sh
+++ pull_terminator_develop.sh
@@ -2,5 +2,3 @@ echo "pulling terminator sources"
 sudo git pull https://github.com/ChinaFred/terminator develop
 echo "refresh over" 
 echo "*******************************************************************************"
-echo "starting server" 
-sudo python terminator/getAlive.py
